8afacab983d232eca7e74ba60c2f6e15a6c986bd
Alex Weisberger <alex.weisberger@viewthespace.com>
2017-03-18 22:24:48 -0400

    Render black cards in color

--- lib/freecell/ncurses_ui.rb
+++ lib/freecell/ncurses_ui.rb
@@ -4,8 +4,11 @@ require_relative 'move_parser'
 module Freecell
   # Commandline UI
   class NCursesUI
+    BLACK_CARD_COLOR_PAIR = 1
+
     def initialize
       @move_parser = MoveParser.new
+      @curr_y = 0
     end
 
     def setup
@@ -13,17 +16,28 @@ module Freecell
       Curses.cbreak
       Curses.noecho
       Curses.nonl
+      setup_color
     ensure
       Curses.close_screen
+    end
+
+    def setup_color
+      Curses.start_color
+      Curses.init_pair(
+        BLACK_CARD_COLOR_PAIR,
+        Curses::COLOR_CYAN,
+        Curses::COLOR_BLACK
+      )
     end
 
     def render(game_state)
       Curses.clear
       render_top_area(game_state)
-      Curses.setpos(4, 0)
+      advance_y(by: 4)
       render_cascades(game_state, 4)
 
       Curses.refresh
+      reset_state
     end
 
     def parse_input
@@ -36,6 +50,10 @@ module Freecell
     end
 
     private
+
+    def reset_state
+      @curr_y = 0
+    end
 
     def render_top_area(game_state)
       render_free_cells(game_state)
@@ -55,22 +73,31 @@ module Freecell
     end
 
     def render_cascades(game_state, start_y)
-      current_y = start_y
       game_state.printable_card_grid.each_with_index do |row, i|
-        Curses.addstr(row.map(&:to_s).join('   '))
-        current_y = start_y + i
-        Curses.setpos(current_y, 0)
+        row.each { |card| draw_card(card) }
+        advance_y(by: 1)
       end
-      Curses.setpos(current_y + 2, 0)
+      advance_y(by: 2)
       game_state.cascades.length.times do |i|
         Curses.addstr(" #{i_to_cascade_letter(i)}    ")
       end
     end
 
-
     def i_to_cascade_letter(i)
       ascii_a = 97
       [i + ascii_a].pack('c*')
+    end
+
+    def draw_card(card)
+      Curses.attron(Curses.color_pair(BLACK_CARD_COLOR_PAIR)) if card.respond_to?(:black?) && card.black?
+      Curses.addstr(card.to_s)
+      Curses.attroff(Curses.color_pair(BLACK_CARD_COLOR_PAIR)) if card.respond_to?(:black?) && card.black?
+      Curses.addstr('   ')
+    end
+
+    def advance_y(by:)
+      @curr_y += by
+      Curses.setpos(@curr_y, 0)
     end
   end
 end
