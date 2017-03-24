56088b1be925f586cc2257f00722f4c722ec5475
Alex Weisberger <alex.weisberger@viewthespace.com>
2017-03-23 22:01:25 -0400

    Closer to original game (#7)

--- lib/freecell/ncurses_ui.rb
+++ lib/freecell/ncurses_ui.rb
@@ -65,10 +65,14 @@ module Freecell
     end
 
     def render_top_area(game_state)
+      Curses.addstr('space')
+      Curses.setpos(@curr_y, 39)
+      Curses.addstr('enter')
+      advance_y(by: 1)
       render_free_cells(game_state)
       Curses.setpos(@curr_y, 21)
       Curses.addstr('=)')
-      Curses.setpos(@curr_y, 23)
+      Curses.setpos(@curr_y, 24)
       render_foundations(game_state)
     end
 
@@ -94,14 +98,14 @@ module Freecell
 
     def render_cascades(game_state, start_y)
       game_state.printable_card_grid.each_with_index do |row, i|
-        Curses.addstr('  ')
+        Curses.addstr('   ')
         row.each do |card|
           draw_card(card)
         end
         advance_y(by: 1)
       end
       advance_y(by: 1)
-      Curses.setpos(@curr_y, 3)
+      Curses.setpos(@curr_y, 4)
       game_state.cascades.length.times do |i|
         Curses.addstr("#{i_to_cascade_letter(i)}    ")
       end
